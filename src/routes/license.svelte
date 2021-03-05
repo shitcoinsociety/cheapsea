a8700abf3c79ce03897451d48475d27711a31c67
Stefan Buhrmester <buhrmi@gmail.com>
2021-03-05 20:08:19 +0000

    Create license.svelte

---
+++ src/routes/license.svelte
@@ -0,0 +1,5 @@
+<h1>License information</h1>
+
+Licenses to run an instance of CheapSea against your own network are now for sale.<br>
+<br>
+Please contact <a href="mailto:sales@shitcoinsociety.com">sales@shitcoinsociety.com</a> for a quote.
