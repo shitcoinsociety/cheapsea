bc2a06d6f49c74397043a8c69b5dacc17b436cd6
buhrmi <buhrmi@gmail.com>
2021-03-05 11:07:25 +0000

    prev/next page

--- src/routes/collections/[collection]/index.svelte
+++ src/routes/collections/[collection]/index.svelte
@@ -17,26 +17,44 @@
   import collections from '../../../collections'
   import NFT from './_nft.svelte'
   import { stores } from '@sapper/app';
-  
+
   export let totalSupply
 
-  let startIndex = 0
-  let perPage = 12
-  const { page } = stores();
-  const url = collections[$page.params.collection].url
+  function nav(params) {
+    const oldParams = $page.params
+    const newParams = Object.assign(oldParams, params)
+    
+  }
 
+  const { page } = stores();
+
+  let perPage = 12
+  const url = collections[$page.params.collection].url
+  
+  $: currentPage = Number($page.query.page) || 0
+  $: startIndex = currentPage * perPage
+  $: isLastPage = (currentPage + 1) * perPage >= totalSupply
+  $: isFirstPage = (currentPage - 1) * perPage < 0
 </script>
 
 <h1>{$page.params.collection}</h1>
 
 <div class="details">
-Visit <a target="_blank" href="{url}">{url}</a> for more information
+  Visit <a target="_blank" href="{url}">{url}</a> for more information
 </div>
 
-Showing {startIndex} - {startIndex + perPage} of {totalSupply}
 
-{#each Array(perPage) as _, i}
-  <NFT collection={$page.params.collection} index={i + 1} />
+Showing {startIndex} - {startIndex + perPage} of {totalSupply}<br>
+{#if !isFirstPage}
+  <a href="/collections/{$page.params.collection}?page={currentPage - 1}">Prev Page</a>
+{/if}
+{#if !isLastPage}
+  <a href="/collections/{$page.params.collection}?page={currentPage + 1}">Next Page</a>
+{/if}
+
+
+{#each Array(perPage) as _, i (i + startIndex + 1)}
+  <NFT collection={$page.params.collection} index={i + startIndex + 1} />
 {/each}
 
 
